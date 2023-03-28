da712e9b62882883a2f93421d5e4349543442510
sytzez <sytzez@gmail.com>
2023-03-28 19:57:15 +0100

    Test recipeStep schema

--- src/schemata/recipe-step.ts
+++ src/schemata/recipe-step.ts
@@ -18,3 +18,25 @@ export const recipeStepSchema = lazy((step) => {
 })
 
 export type RecipeStep = ActionStep | AddIngredientStep
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+
+  it('can validate an action step', () => {
+    const step = { type: 'action', description: 'Do something' }
+
+    expect(recipeStepSchema.validateSync(step)).toEqual(step)
+  })
+
+  it('can validate an ingredient step', () => {
+    const step = { type: 'add-ingredient', ingredientId: 1, quantity: 2.5 }
+
+    expect(recipeStepSchema.validateSync(step)).toEqual(step)
+  })
+
+  it('is invalid with an unknown type', () => {
+    const step = { type: 'unknown-step' }
+
+    expect(() => recipeStepSchema.validateSync(step)).toThrow()
+  })
+}
